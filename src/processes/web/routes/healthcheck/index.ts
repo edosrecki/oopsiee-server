144fbb50421edb5013af5b1b3d2577a3d0f19ce0
Dinko Osrecki <dinko.osrecki@emarsys.com>
2019-10-11 13:53:01 +0200

    docs(README): specify oopsiee-server features

--- src/processes/web/routes/healthcheck/index.ts
+++ src/processes/web/routes/healthcheck/index.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance, FastifyOptions, FastifyNext } from 'fastify'
-import { getHealthcheckHandler } from './healthcheck'
+import { getHealthcheckHandler } from './healthcheck.get'
 
 export function healthcheckRoutes (fastify: FastifyInstance, options: FastifyOptions, next: FastifyNext) {
   fastify.get('/healthcheck', getHealthcheckHandler)
