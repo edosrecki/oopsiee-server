74af574e68e04a175b0a6aba8bee91159e24686e
Dinko Osrecki <dinko.osrecki@emarsys.com>
2019-10-12 23:17:24 +0200

    test(example.oopsiee): add procedure tests

---
+++ src/procedures/example/oopsiee.test.ts
@@ -0,0 +1,28 @@
+import { oopsiee } from './oopsiee'
+import { buildLogger } from '../../util/test-mocks'
+
+describe('procedures:example:oopsiee', () => {
+  const params = { key: 'value' }
+  const context = { user: 'user' }
+
+  // ~~~~
+
+  test('log params to info level', async () => {
+    const logger = buildLogger()
+    const procedure = oopsiee({ logger })
+
+    await procedure(params, context)
+
+    expect(logger.info).toBeCalledTimes(1)
+    expect(logger.info).toBeCalledWith({ params }, 'procedure: example.oopsiee')
+  })
+
+  test('return hello message', async () => {
+    const logger = buildLogger()
+    const procedure = oopsiee({ logger })
+
+    const result = await procedure(params, context)
+
+    expect(result).toEqual({ message: 'Hello, user!' })
+  })
+})
