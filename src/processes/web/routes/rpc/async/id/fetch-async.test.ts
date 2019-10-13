cbb1b82eacbb91ff9893be726cf76eeab2ade497
Dinko Osrecki <dinko.osrecki@emarsys.com>
2019-10-13 23:55:42 +0200

    test(web): add tests for rpc/fetch-async

---
+++ src/processes/web/routes/rpc/async/id/fetch-async.test.ts
@@ -0,0 +1,60 @@
+import { asValue, createContainer } from 'awilix'
+import { HTTPInjectOptions } from 'fastify'
+import { Container } from '../../../../../../container'
+import { buildServer } from '../../../../build-server'
+import { injectAuthRequest, injectRequest } from '../../../../../../util/test/inject-request'
+
+describe('routes.rpc.fetch-async', () => {
+
+  const request: HTTPInjectOptions = {
+    method: 'GET',
+    url: 'rpc/async/1',
+  }
+
+  const buildContainer = (): Container => {
+    const container = createContainer()
+    container.register('config', asValue({}))
+    return container
+  }
+
+  // ~~~~
+
+  it(`return '401 Unauthorized' if user is not authenticated`, async () => {
+    const container = buildContainer()
+    const server = buildServer(container)
+
+    const response = await injectRequest(server, request)
+
+    expect(response.statusCode).toBe(401)
+  })
+
+  it(`return '404 Not Found' if job does not exist`, async () => {
+    const container = buildContainer()
+    const server = buildServer(container)
+
+    const queue = { getJob: jest.fn() }
+    queue.getJob.mockResolvedValueOnce(null)
+    container.register('queue', asValue(queue))
+
+    const response = await injectAuthRequest(server, request)
+
+    expect(response.statusCode).toBe(404)
+    expect(response.payload.message).toBe('Job #1 not found.')
+    expect(queue.getJob).toBeCalledWith('1')
+  })
+
+  it('return job result', async () => {
+    const container = buildContainer()
+    const server = buildServer(container)
+
+    const job = { data: { result: { foo: 'bar' } } }
+    const queue = { getJob: jest.fn() }
+    queue.getJob.mockResolvedValueOnce(job)
+    container.register('queue', asValue(queue))
+
+    const response = await injectAuthRequest(server, request)
+
+    expect(response.statusCode).toBe(200)
+    expect(response.payload).toEqual(job.data.result)
+  })
+})
